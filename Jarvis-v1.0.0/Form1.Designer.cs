0d756f25b3aeec7c90721be722b50d7da152c792
TeofilSturzoiu <teofil.sturzoiu@gmail.com>
2022-03-19 15:02:33 +0200

    Virtual assistant

---
+++ Jarvis-v1.0.0/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Jarvis_v1._0._0
+{
+    partial class Jarvis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Jarvis));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.richTextBox1.Location = new System.Drawing.Point(1195, 477);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(237, 331);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Moccasin;
+            this.label1.Location = new System.Drawing.Point(1324, 454);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Live Questions";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Moccasin;
+            this.button1.Location = new System.Drawing.Point(1195, 444);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 113);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1015, 519);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Jarvis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1440, 878);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Jarvis";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Jarvis-v1.0.0/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+//using Google.API.Search;
+
+namespace Jarvis_v1._0._0
+{
+    public partial class Jarvis : Form
+    {
+        SpeechRecognitionEngine Speechreco = new SpeechRecognitionEngine();
+        SpeechSynthesizer jarvis = new SpeechSynthesizer();
+        DateTime dateTime = DateTime.UtcNow.Date;
+
+        public Jarvis()
+        {
+            Speechreco.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Speechreco_SpeechRecognized);
+            LoadGrammar();
+            Speechreco.SetInputToDefaultAudioDevice();
+            Speechreco.RecognizeAsync(RecognizeMode.Multiple);       //Sa revin aici pentru comenzi luate gresit.
+            jarvis.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen);
+            //jarvis.SelectVoice("Microsoft Server Speech Text to Speech Voice (en-US, Helen)");
+            InitializeComponent();
+        }
+
+        private void Speechreco_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            
+            richTextBox1.Text = e.Result.Text;
+            string speech = e.Result.Text;
+            //jarvis.Speak("Au trecut 15 minute.");
+            switch(speech)
+            {
+                case "Jarvis, hello":
+                case "Jarvis, hi":
+                    jarvis.Speak("Hello Sir.");
+                    break;
+                case "Jarvis how are you?":
+                    jarvis.Speak("I am fine Sir.");
+                    break;
+                case "Jarvis what is the date?":
+                    jarvis.Speak("Today is" + dateTime);
+                    break;
+                case "Jarvis Open browser for me":
+                    jarvis.Speak("Wait a second");
+                    System.Diagnostics.Process.Start("http://google.com");
+                    break;
+                case "Jarvis End process":
+                    jarvis.Speak("See you soon Sir");
+                    Application.Exit();
+                    break;
+                case "What is your name?":
+                    jarvis.Speak("My name is Jarvis, and I am your servant");
+                    break;
+                case "Jarvis Who created you?":
+                    jarvis.Speak("Teo created me.");
+                    break;
+                case "Jarvis Play a movie for me":
+                    jarvis.Speak("Give me a second");
+                    System.Diagnostics.Process.Start(@"D:\FILME\All.Marvel.Movies.Related.To.Infinity.Stones.In.Chronological.Order.HDRO-NoGrp\17.Black.Panther.2018.BluRay.720p.DTS.x264-MTeam\Black.Panther.2018.BluRay.720p.DTS.x264-MTeam.mkv");
+                    break;
+                case "Jarvis what day is it":
+                    string day;
+                    day = "Today is, " + System.DateTime.Now.ToString("dddd");
+                    jarvis.SpeakAsync(day);
+                    break;
+                case "what time is it":
+                    System.DateTime now = System.DateTime.Now;
+                    string time = now.GetDateTimeFormats('t')[0];
+                    jarvis.Speak(time);
+                    break;
+                case "who are you":
+                    jarvis.Speak("i am your personal assistant");
+                    jarvis.Speak("i can read email, weather report, i can search web for you, anything that you need a personal assistant to do, you can ask me a question and I will reply to you");
+                    break;
+                case "test":
+                    string urladdress = e.Result.Text;
+                    webBrowser1.Navigate("www.google.com/search" + urladdress);
+                    jarvis.Speak("done");
+                    break;
+            }
+        }
+
+        private void LoadGrammar()
+        {
+            Choices texts = new Choices();
+            string[] lines = File.ReadAllLines(Environment.CurrentDirectory + "\\commands.txt");
+            texts.Add(lines);
+            Grammar wordlist = new Grammar(new GrammarBuilder(texts));
+            Speechreco.LoadGrammar(wordlist);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
